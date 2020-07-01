927ea799d00499d51a045a23c5edaff9425b2688
shersadriddinov <sher.sadriddinov@gmail.com>
2020-07-01 22:55:22 +0500

    websocket server init

--- main.py
+++ main.py
@@ -1,12 +1,17 @@
 from sanic import Sanic
-from sanic.response import json
+from sanic.websocket import WebSocketProtocol
 
 app = Sanic("cyber_socket")
 
 
-@app.route("/lobby")
-async def test(request):
-	return json({"hello": "World"})
+@app.websocket('/lobby')
+async def feed(request, ws):
+	while True:
+		data = 'hello!'
+		print('Sending: ' + data)
+		await ws.send(data)
+		data = await ws.recv()
+		print('Received: ' + data)
 
 if __name__ == "__main__":
-	app.run(host="0.0.0.0", port=8000)
+	app.run(host="0.0.0.0", port=8000, protocol=WebSocketProtocol)
