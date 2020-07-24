a32821116f9adc9c191783a786a8ea1c7df60ae5
shersadriddinov <sher.sadriddinov@gmail.com>
2020-07-24 23:50:15 +0500

    logger configured

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from sanic import Sanic
+from sanic.log import logger
 from sanic.websocket import WebSocketProtocol
 import ujson as json
 
@@ -16,47 +17,63 @@ CONNECTIONS = []
 async def connect(request, ws):
 	running = True
 	connection = Connection(ws=ws)
+	logger.info("New connection from " + request.ip)
 	CONNECTIONS.append(connection)
 
 	while running:
 		in_data = await ws.recv()
 		data = json.loads(in_data)
 		action = data.get('action', False)
+		uuid = data.get("uuid")
 		# Connection routine
+		logger.info(request.ip + "'s action is [" + action + "]")
 		if action == 'connect':
 			# TODO: write clear docs
-			connection.uuid = data.get("uuid")
+			connection.uuid = uuid
 			connection.status = "ONLINE"
 			connection.party_status = True
-			connection.party_id = data.get("uuid")
+			connection.party_id = uuid
 			# TODO: notify every one in connection list that user is ONLINE
 			# TODO: update player count
-
+			for user in CONNECTIONS:
+				if user.uuid == uuid:
+					try:
+						await user.ws.send(json.dumps({
+								"action": "connect",
+								"error": 0
+						}))
+						logger.info(request.ip + "'s [" + action + "] success")
+					except:
+						logger.error(request.ip + "'s [" + action + "] failed")
 		# Notification routine
-		if action == 'notification':
-			print('hello')
+		elif action == 'notification':
 			if data.get('type', 0) == 1:
 				for user in CONNECTIONS:
-					if user.uuid == data.get("uuid", False):
+					if user.uuid == uuid:
 						try:
 							await user.ws.send(json.dumps({
 								"action": "notification"
 							}))
+							logger.info(request.ip + "'s [" + action + "] success")
 						except:
-							pass
-		if action == 'friend_request_confirm':
+							logger.error(request.ip + "'s [" + action + "] failed: cannot send")
+		elif action == 'friend_request_confirm':
+			friend = data.get("friend", False)
 			for user in CONNECTIONS:
-				if user.uuid == data.get("uuid", False):
+				if user.uuid == uuid:
 					try:
 						await user.ws.send(json.dumps({
 							"action": "friend_confirmed",
-							"uuid": data.get("uuid"),
-							"friend": data.get("friend")
+							"uuid": uuid,
+							"friend": friend
 						}))
+						logger.info("Friend Request Confirm sent from " + friend + " to " + uuid)
 					except:
-						pass
+						logger.error(request.ip + "'s [" + action + "] failed: cannot send")
+			else:
+				logger.error("User with uuid=[" + uuid + "] is not online")
 
 	CONNECTIONS.remove(connection)
 
 if __name__ == "__main__":
-	app.run(host="0.0.0.0", port=8003, protocol=WebSocketProtocol)
+	app.run(host="0.0.0.0", port=8003, protocol=WebSocketProtocol, debug=True, access_log=True)
